4a4cca04d6a8b3750566d6dafc6a0aeada471443
VeryExcitedGremlin <oder.ct@gmail.com>
2025-11-03 17:31:49 -0800

    add HiddenNavigation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route, Routes} from 'react-router';
 import DisplayHello from "./components/module-47-react-basics/display-47/Display47";
 import DisplayCounters from "./components/module-49-state-management/display-49/Display49";
 import { PersonalFooter } from "./components/Footer/personal-footer";
+import HiddenNavigation from "./components/HiddenNavigation/HiddenNav";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/first-react-app/" element={<HiddenNavigation />} />
         <Route path="/first-react-app/47" element={<DisplayHello />} />
         <Route path="/first-react-app/49" element={<DisplayCounters />} />
       </Routes>
